3ee10130d4612118521d34364d0173d4b356ee77
Pickle1337 <m1ndyymain@gmail.com>
2024-05-13 17:28:12 +0200

    Add files via upload

---
+++ inj/main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string_view>
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <memory>
+#include <cstdint>
+#include <vector>
+#include "injector.hpp"
+#include "lazy.h"
+#include "protection/antiDbg.h"
+
+
+int main()
+{
+	
+																																																							აპლიკაცია.init();
+MessageBox(NULL, "press in lobby", "Inject", MB_OK | MB_ICONINFORMATION);
+
+pysen(E("UnrealWindow"),L"injected");
+
+
+
+	exit(0);
+}
+
